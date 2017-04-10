8c9bec481ce2a33c0b4c6c7f8f062868075b63fa
Oka bRionZ <okabrionz@gmail.com>
2017-04-11 02:14:34 +0700

    maximum length

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EXAMPLE.COM';
-  desc = 'Consequat cupidatat exercitation minim sint sunt eiusmod sint do. Lorem culpa adipisicing sit consectetur ipsum dolore excepteur fugiat do voluptate incididunt. Qui deserunt Lorem id et. Qui voluptate aliquip occaecat laborum voluptate qui mollit id cupidatat fugiat eiusmod in.';
+  desc = 'Consequat cupidatat exercitation minim sint sunt eiusmod sint do.';
   cardtitle = 'Domain Name Invesment';
   cardimg = '/assets/img/water.jpg';
   cardtext = 'Cillum ipsum sunt cupidatat in est cupidatat ipsum velit exercitation aliqua.';
